157a3300ceffa009dfa586a0a2fed800f4711ccb
JAGDISH KOMAKULA <jagxlnc@gmail.com>
2019-11-13 14:54:19 +0530

    First Commit

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <html>
 
 <head>
-<title>Hello World! IBM</title>
+<title>Hello World!</title>
 </head>
 
 <body>
 
-<?php echo "Hello World! JAGDISH"; ?>
+<?php echo "Hello World!"; ?>
 <?php if($_ENV["HOSTNAME"]) {?><h3>My hostname is <?php echo $_ENV["HOSTNAME"]; ?><br /><br />
 
 <?php $links = [];
